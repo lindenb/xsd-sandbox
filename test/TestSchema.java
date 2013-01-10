8390384be77e2d7a3ea67b65feadceb4ef0dd668
lindenb <plindenbaum@yahoo.fr>
2013-01-10 13:28:47 +0100

    doid

--- test/TestSchema.java
+++ test/TestSchema.java
@@ -1,7 +1,5 @@
 import java.io.InputStream;
 import java.io.StringWriter;
-import org.geneontology.dtds.go.*;
-import org.w3._1999._02._22_rdf_syntax_ns_.*;
 import javax.xml.namespace.QName;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
@@ -13,18 +11,27 @@ import javax.xml.validation.SchemaFactory;
 import javax.xml.XMLConstants;
 
 
-public class TestGo
+public class TestSchema
     {
     public static void main(String[] args) throws Exception
         {
-        SchemaFactory factory=SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-        Schema schema= factory.newSchema(new StreamSource(new java.io.File("../schemas/bio/go/go.xsd")));
-	JAXBContext jaxbCtxt=JAXBContext.newInstance("org.geneontology.dtds.go:org.w3._1999._02._22_rdf_syntax_ns_");
+        if(args.length!=3)
+        	{
+        	System.err.println("Usage  (schema.xsd or 'null') (packages) file.xml");
+        	System.exit(-1);
+        	}
+       
+	JAXBContext jaxbCtxt=JAXBContext.newInstance(args[1]);
 	Marshaller marshaller = jaxbCtxt.createMarshaller();
 	Unmarshaller unmarshaller=jaxbCtxt.createUnmarshaller();
         marshaller.setProperty("jaxb.formatted.output",true);
-        unmarshaller.setSchema(schema);
-        Object go=unmarshaller.unmarshal(new java.io.File("go.xml"));
-        marshaller.marshal(go, System.out);
+        if(!args[0].equals("null"))
+        	{
+		SchemaFactory factory=SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+		Schema schema= factory.newSchema(new StreamSource(new java.io.File(args[0])));
+		unmarshaller.setSchema(schema);
+		}
+        Object o=unmarshaller.unmarshal(new java.io.File(args[2]));
+        marshaller.marshal(o, System.out);
         }
     }
